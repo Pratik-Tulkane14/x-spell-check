0aa60880050e7eb1a0136ba3c8f5d587a214683e
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-03-19 18:13:47 +0530

    fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,26 @@ function App() {
     exampl: "example",
   };
   const [input, setInput] = useState("");
-  const [suggestionText, setSuggestionText] = useState('');
+  const [suggestionText, setSuggestionText] = useState("");
   const handleChange = (e) => {
     const text = e.target.value;
     setInput(text);
     const words = text.split(" ");
-    const correctedWords = words.map((word)=>{
+    const correctedWords = words.map((word) => {
       const correctedWord = customDictionary[word.toLowerCase()];
-      return correctedWord || "";
-    })
+      return correctedWord || word;
+    });
     console.log(correctedWords, "correctedWords");
-    
+
     const firstCorrection = correctedWords.find(
-      (word, index) => word !== word[index]
+      (word, index) => word !== words[index]
     );
-    setSuggestionText(firstCorrection ||'');
+    setSuggestionText(firstCorrection || "");
   };
   return (
     <>
       <h1>Spell Check & Auto-Correction</h1>
-      <textarea 
+      <textarea
         name="inputText"
         id="input-area"
         placeholder="Enter text..."
