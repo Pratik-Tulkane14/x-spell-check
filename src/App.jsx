bdc1a862b4b64991452160db73f85af135c14f2e
Pratik-Tulkane14 <pratiktulkane12345@gmail.com>
2025-03-22 16:03:33 +0530

    fix the error

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
     const firstCorrection = correctedWords.find(
       (word, index) => word !== words[index]
     );
-    // setSuggestionText(firstCorrection || "");
-    setTimeout(() => {
-      setSuggestionText(firstCorrection || "");
-    }, 0);
+    setSuggestionText(firstCorrection || "");
+    // setTimeout(() => {
+    //   setSuggestionText(firstCorrection || "");
+    // }, 0);
   };
   return (
     <>
